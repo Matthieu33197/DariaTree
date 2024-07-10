5941d858fdd41c8e11023015939d5bb8fe121640
Matthieu Queru <75125755+Matthieu33197@users.noreply.github.com>
2024-07-10 18:35:11 +0900

    [ADD] second design without kuromi

--- dariatree/src/app/components/tree.tsx
+++ dariatree/src/app/components/tree.tsx
@@ -12,15 +12,15 @@ const TreeLink: React.FC<TreeLinkProps> = ({ href, title, description }) => {
       href={href}
       target="_blank"
       rel="noopener noreferrer"
-      className="group w-full max-w-sm md:max-w-md lg:max-w-lg rounded-lg border border-gray-300 bg-white px-4 py-3 transition-colors hover:border-gray-400 hover:bg-gray-100 dark:border-neutral-700 dark:bg-neutral-800 dark:hover:border-neutral-600 dark:hover:bg-neutral-700"
+      className="group w-full max-w-sm md:max-w-md lg:max-w-lg rounded-lg border px-4 py-3 transition-colors border-black bg-[#EFA6FB] hover:border-neutral-600 hover:bg-[#A157AD] shadow-xl drop-shadow-2xl hover:shadow-xl"
     >
-      <h2 className="mb-1 text-lg md:text-xl lg:text-2xl font-semibold">
+      <h2 className="mb-1 text-lg md:text-xl lg:text-2xl font-semibold text-black">
         {title}
         <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
           -&gt;
         </span>
       </h2>
-      {description && <p className="m-0 text-xs md:text-sm lg:text-base opacity-75">{description}</p>}
+      {description && <p className="m-0 text-xs md:text-sm lg:text-base opacity-75 text-black">{description}</p>}
     </a>
   );
 };
--- dariatree/src/app/page.tsx
+++ dariatree/src/app/page.tsx
@@ -3,33 +3,42 @@ import TreeLink from '../app/components/tree';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-4 md:p-8 bg-gray-900">
-      <div className="w-full max-w-5xl flex flex-col items-center justify-center text-center space-y-4 md:space-y-6">
-        <div className="w-24 h-24 md:w-32 md:h-32 rounded-full overflow-hidden">
-          <Image
-            className="object-cover w-full h-full"
-            src="/profil.PNG" // Remplacez par votre photo de profil
-            alt="Profile Picture"
-            width={128} // Taille de l'image en pixels pour correspondre aux dimensions du conteneur
-            height={128}
-            priority
-          />
+    <div className="relative overflow-hidden min-h-screen bg-[#AB89D7]">
+      <main className="relative flex min-h-screen flex-col items-center justify-between p-4 md:p-8">
+        <div className="absolute -top-36 left-30 w-72 h-72 bg-[#362A66] rounded-full shadow-lg z-0"></div>
+        <div className="absolute -top-10 -left-10 w-32 h-32 bg-[#362A66] rounded-full shadow-lg drop-shadow-2xl z-10"></div>
+        <div className="absolute top-60 -right-4 w-32 h-32 bg-[#362A66] rounded-full shadow-lg drop-shadow-2xl z-10"></div>
+        <div className="absolute -bottom-10 -right-4 w-40 h-40 bg-[#362A66] rounded-full shadow-lg drop-shadow-2xl z-0"></div>
+        <div className="absolute bottom-14 -right-6 w-32 h-32 bg-[#362A66] rounded-full shadow-lg drop-shadow-2xl z-10"></div>
+        <div className="absolute bottom-52 -left-10 w-32 h-32 bg-[#362A66] rounded-full shadow-lg drop-shadow-2xl z-0"></div>
+        <div className="absolute bottom-32 -left-14 w-40 h-40 bg-[#362A66] rounded-full shadow-lg drop-shadow-2xl z-10"></div>
+        <div className="relative w-full max-w-5xl flex flex-col items-center justify-center text-center space-y-4 md:space-y-6 z-20 top-6">
+          <div className="w-24 h-24 md:w-32 md:h-32 rounded-full overflow-hidden">
+            <Image
+              className="object-cover w-full h-full"
+              src="/profil.PNG" // Remplacez par votre photo de profil
+              alt="Profile Picture"
+              width={128} // Taille de l'image en pixels pour correspondre aux dimensions du conteneur
+              height={128}
+              priority
+            />
+          </div>
+          <h1 className="text-2xl md:text-3xl font-bold text-black">Deathless.3</h1>
+          <p className="text-sm md:text-base text-black">Welcome to my link tree. Follow me on my socials and check out my latest content!</p>
         </div>
-        <h1 className="text-2xl md:text-3xl font-bold text-white">_deathless.3</h1>
-        <p className="text-sm md:text-base text-gray-300">Welcome to my link tree. Follow me on my socials and check out my latest content!</p>
-      </div>
 
-      <div className="w-full max-w-5xl flex flex-col items-center justify-center space-y-3 md:space-y-4 mt-8 md:mt-10">
-        <TreeLink href="https://www.instagram.com/_deathless.3?igsh=MWJ6eXRtZng0a2NwbA%3D%3D&utm_source=qr" title="Instagram" description="Follow me on Instagram" />
-        <TreeLink href="https://youtube.com/@deathless.3?si=rzb7tuHZvngasPYZ" title="YouTube" description="Subscribe to my YouTube channel" />
-        <TreeLink href="https://bead-beginner-30b.notion.site/1-a73a720c17494ad5af8e3c55be5c003d?pvs=4" title="Lesson #1" description="1st free lesson Ukr" />
-        <TreeLink href="https://t.me/koreanfrogs" title="Telegram" description="Join my telegram channel to get all updates on my lesson" />
-        <TreeLink href="https://www.tiktok.com/@deathless_.3?_t=8ntshtbkNex&_r=1" title="TikTok" description="Check out my TikTok videos" />
-      </div>
+        <div className="relative w-full max-w-5xl flex flex-col items-center justify-center space-y-3 md:space-y-4 mt-8 md:mt-10 z-20 top-6">
+          <TreeLink href="https://www.instagram.com/_deathless.3?igsh=MWJ6eXRtZng0a2NwbA%3D%3D&utm_source=qr" title="Instagram" description="Follow me on Instagram" />
+          <TreeLink href="https://youtube.com/@deathless.3?si=rzb7tuHZvngasPYZ" title="YouTube" description="Subscribe to my YouTube channel" />
+          <TreeLink href="https://bead-beginner-30b.notion.site/1-a73a720c17494ad5af8e3c55be5c003d?pvs=4" title="Lesson #1" description="1st free lesson Ukr" />
+          <TreeLink href="https://t.me/koreanfrogs" title="Telegram" description="Join my telegram channel to get all updates on my lesson" />
+          <TreeLink href="https://www.tiktok.com/@deathless_.3?_t=8ntshtbkNex&_r=1" title="TikTok" description="Check out my TikTok videos" />
+        </div>
 
-      <footer className="w-full max-w-5xl flex items-center justify-center py-4">
-        <p className="text-sm md:text-base text-gray-300">© 2024 _deathless.3. All rights reserved.</p>
-      </footer>
-    </main>
+        <footer className="relative w-full max-w-5xl flex items-center justify-center py-4 z-20 top-6">
+          <p className="text-sm md:text-base text-black">© 2024 _deathless.3. All rights reserved.</p>
+        </footer>
+      </main>
+    </div>
   );
 }
